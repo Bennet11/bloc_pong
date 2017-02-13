22971de888f240e3c07b86cffa40ab17a876213c
Bennet11 <bennetveloya@gmail.com>
2017-02-13 13:23:32 -0800

    some refractoring on initial commit but still not rendering

--- scripts/script.js
+++ scripts/script.js
@@ -1,51 +1,60 @@
-function Paddle(xPos, yPos, width, height) {
-  this.xPosition = xPos;
-  this.yPosition = yPos;
+function Paddle(x, y, width, height) {
+  this.x = x;
+  this.y = y;
   this.width = width;
   this.height = height;
 }
 
-function Ball(xPos, yPos, radius) {
-  this.xPosition = xPos;
-  this.yPosition = yPos;
+function Ball(x, y, radius) {
+  this.x = x;
+  this.y = y;
   this.radius = radius
 }
 
 function Player() {
-  this.paddle = new Paddle(795, 490, 8, 70);
+  this.paddle = new Paddle(5, 225, 8, 70);
 }
 
 function Computer() {
-  this.paddle = new Paddle(5, 10, 8, 70);
+  this.paddle = new Paddle(795, 275, 8, 70);
 }
 
 Paddle.prototype.render = function(context) {
   context.beginPath();
-  context.rect(this.xPosition, this.yPosition, this.width, this.height);
+  context.rect(this.x, this.y, this.width, this.height);
   context.fillStyle = "white";
   context.fill();
-}
+};
 
 Ball.prototype.render = function(context) {
   context.beginPath();
-  context.arc(this.xPosition, this.yPosition, this.radius);
+  context.arc(this.x, this.y, this.radius);
   context.strokeStyle = "black";
   context.stroke();
   context.fillStyle = "black";
   context.fill();
-}
+};
 
-function render(context) {
-  player.render(context);
-  computer.render(context);
-  ball.render(context);
-}
+Player.prototype.render = function() {
+  this.paddle.render();
+};
 
+Computer.prototype.render = function() {
+  this.paddle.render();
+};
 
 var player = new Player();
 var computer = new Computer();
-var ball = new Ball(400, 275, 10);
+var ball = new Ball(400, 250, 10);
 
-window.onload = function (context) {
+function render() {
+  player.render();
+  computer.render();
+  ball.render()
+}
+
+window.onload = function () {
+  var canvas = document.getElementById("myPong");
+  var context = canvas.getContext("2d");
   render(context)
 }
