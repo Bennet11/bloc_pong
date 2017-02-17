dfb61a5a0c02a49e44e2b531fe37853a12ff2eef
Bennet11 <bennetveloya@gmail.com>
2017-02-16 16:29:26 -0800

    some progress but still needs work

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,25 @@
+var animate = window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame    ||
+        window.oRequestAnimationFrame      ||
+        window.msRequestAnimationFrame     ||
+        function(callback) { window.setTimeout(callback, 1000/60) };
+
+var canvas = document.getElementById("myPong");
+var context = canvas.getContext("2d");
+var player = new Player();
+var computer = new Computer();
+var ball = new Ball(400, 250, 10);
+var keyDown = {};
+
+
 function Paddle(x, y, width, height) {
   this.x = x;
   this.y = y;
   this.width = width;
   this.height = height;
+  this.xspeed = 0;
+  this.yspeed = 0;
 }
 
 function Ball(x, y, radius) {
@@ -26,6 +43,19 @@ Paddle.prototype.render = function(context) {
   context.fill();
 };
 
+Paddle.prototype.move = function(x, y) {
+  this.x += x;
+  this.y += y;
+  this.xspeed = x;
+  this.yspeed = y;
+  if (this.x < 0) {
+    this.x = 0;
+    this.xspeed = 0;
+  } else if () {
+
+  }
+};
+
 Ball.prototype.render = function(context) {
   context.beginPath();
   context.arc(40, 40, this.radius, 0, 2*Math.PI );
@@ -43,18 +73,24 @@ Computer.prototype.render = function(context) {
   this.paddle.render(context);
 };
 
-var player = new Player();
-var computer = new Computer();
-var ball = new Ball(400, 250, 10);
-
 function render(context) {
   player.render(context);
   computer.render(context);
   ball.render(context);
 }
 
-window.onload = function () {
-  var canvas = document.getElementById("myPong");
-  var context = canvas.getContext("2d");
+function step() {
   render(context);
+  animate(step);
+
+};
+
+window.onload = function (keyCode) {
+  window.addEventListener('keydown', function(event) {
+    keyDown(event.keyCode) = true;
+  });
+
+  window.addEventListener('keyup', function(event) {
+    keyDown(event.keyCode) = false;
+  });
 }
